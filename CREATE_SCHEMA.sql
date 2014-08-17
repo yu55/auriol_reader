cc83b7bc9438b09fb39ca86ac49f91939ceacde8
Marcin P <pi@raspberrypi.(none)>
2014-08-17 22:25:13 +0200

    Add anemometer table with temperature to schema SQL.

--- CREATE_SCHEMA.sql
+++ CREATE_SCHEMA.sql
@@ -2,3 +2,8 @@ CREATE TABLE pluviometer(
     created DATETIME,     -- date and time when transmission from pluviometer occured
     amount  DECIMAL(10,2) -- amount of rain in mm transmitted from pluviometer
 );
+
+CREATE TABLE anemometer(
+    created DATETIME,     -- date and time when transmission from anemometer occured
+    temperature  DECIMAL(4,2) -- temperature in Celsius degrees transmitted from pluviometer
+);
