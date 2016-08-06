e2f59fe28d0635130b54bbb19209857ec28a2c78
Marcin P <marcin.pil@gmail.com>
2016-08-06 16:50:04 +0200

    indent --linux-style auriol-reader.c db.c db.h

--- auriol-reader.c
+++ auriol-reader.c
@@ -51,8 +51,7 @@ static const char LANG_TRANS_FILE_OPEN_ERR[] =
 static const char LANG_TRANS_FILE_END[] =
     "Reached end of file with transmission data. Terminating. Good-bye!";
 static const char LANG_INFO_PLUVIOMETER[] = "Deszczomierz: %.2f mm";
-static const char LANG_INFO_WIND_AVG[] =
-    "Srednia predkosc wiatru: %.2f m/s";
+static const char LANG_INFO_WIND_AVG[] = "Srednia predkosc wiatru: %.2f m/s";
 static const char LANG_INFO_WIND_DIR_GUST[] =
     "Kierunek wiatru: %i stopni        Poryw: %.2f m/s";
 static const char LANG_INFO_TEMP_HUMIDITY[] =
@@ -83,302 +82,303 @@ unsigned char previousEncodedBitInRange = 0;
 unsigned char recording = 0;
 unsigned char encodedBits[36] =
     { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 };
 
 unsigned char encodedBitsIndex = 0;
 
 int main(int argc, char *argv[])
 {
-    initializeDatabase();
+	initializeDatabase();
 
-    wiringPiSetup();
+	wiringPiSetup();
 
-    printTime();
-    printf(LANG_PROGRAM_TITLE);
+	printTime();
+	printf(LANG_PROGRAM_TITLE);
 
-    while (1) {
-	int level = digitalRead(RECIEVE_PIN);
+	while (1) {
+		int level = digitalRead(RECIEVE_PIN);
 
-	int bitLength = findEncodedBitLength(level);
-	decodeBitLength(bitLength);
+		int bitLength = findEncodedBitLength(level);
+		decodeBitLength(bitLength);
 
-	delayMicroseconds(50);
+		delayMicroseconds(50);
 
-	globalLevelsCounter++;
-    }
-    return 0;
+		globalLevelsCounter++;
+	}
+	return 0;
 }
 
 /* Read GPIO level */
 unsigned char readLevel()
 {
-    int c = fgetc(pFile);
-    if (c != EOF) {
-	return c - '0';
-    } else {
-	puts(LANG_TRANS_FILE_END);
-	exit(2);
-    }
+	int c = fgetc(pFile);
+	if (c != EOF) {
+		return c - '0';
+	} else {
+		puts(LANG_TRANS_FILE_END);
+		exit(2);
+	}
 }
 
 /* Analyze transmission bit after bit */
 int findEncodedBitLength(unsigned char level)
 {
-    levelsCounter++;
+	levelsCounter++;
 
-    /* Out of range */
-    if (levelsCounter >
-	SYNCHRO_LENGTH + LENGTHS_MARGIN + SEPARATOR_LENGTH +
-	LENGTHS_MARGIN) {
-	levelsCounter = 1;
-	resetRecording();
-	previousEncodedBitInRange = 1;
-    }
-
-    int encodedBitLength = -1;
-    if (level == 0) {
-	if (levelOneCounter > SEPARATOR_LENGTH - LENGTHS_MARGIN
-	    && levelOneCounter < SEPARATOR_LENGTH + LENGTHS_MARGIN) {
-	    if (previousEncodedBitInRange) {
-		encodedBitLength = levelsCounter - levelOneCounter;
-	    }
-	    levelsCounter = 0;
-	    previousEncodedBitInRange = 1;
+	/* Out of range */
+	if (levelsCounter >
+	    SYNCHRO_LENGTH + LENGTHS_MARGIN + SEPARATOR_LENGTH +
+	    LENGTHS_MARGIN) {
+		levelsCounter = 1;
+		resetRecording();
+		previousEncodedBitInRange = 1;
 	}
-	levelOneCounter = 0;
-    } else {
-	levelOneCounter++;
-    }
-    return encodedBitLength;
+
+	int encodedBitLength = -1;
+	if (level == 0) {
+		if (levelOneCounter > SEPARATOR_LENGTH - LENGTHS_MARGIN
+		    && levelOneCounter < SEPARATOR_LENGTH + LENGTHS_MARGIN) {
+			if (previousEncodedBitInRange) {
+				encodedBitLength =
+				    levelsCounter - levelOneCounter;
+			}
+			levelsCounter = 0;
+			previousEncodedBitInRange = 1;
+		}
+		levelOneCounter = 0;
+	} else {
+		levelOneCounter++;
+	}
+	return encodedBitLength;
 }
 
 void resetRecording()
 {
-    recording = 0;
-    memset(encodedBits, 2, sizeof(encodedBits[0]) * 36);
-    encodedBitsIndex = 0;
+	recording = 0;
+	memset(encodedBits, 2, sizeof(encodedBits[0]) * 36);
+	encodedBitsIndex = 0;
 }
 
 void decodeBitLength(int length)
 {
-    /* Signal length too short */
-    if (length < ZERO_LENGTH - LENGTHS_MARGIN) {
-	return;
-    }
+	/* Signal length too short */
+	if (length < ZERO_LENGTH - LENGTHS_MARGIN) {
+		return;
+	}
 
-    /* Sync bit - Start of data package */
-    if (length > SYNCHRO_LENGTH - LENGTHS_MARGIN * 2
-	&& length < SYNCHRO_LENGTH + LENGTHS_MARGIN * 2 && !recording) {
-	resetRecording();
-	recording = 1;
+	/* Sync bit - Start of data package */
+	if (length > SYNCHRO_LENGTH - LENGTHS_MARGIN * 2
+	    && length < SYNCHRO_LENGTH + LENGTHS_MARGIN * 2 && !recording) {
+		resetRecording();
+		recording = 1;
 
-	/* One  */
-    } else if (length > ONE_LENGTH - LENGTHS_MARGIN
-	       && length < ONE_LENGTH + LENGTHS_MARGIN && recording) {
-	encodedBits[encodedBitsIndex++] = 1;
+		/* One  */
+	} else if (length > ONE_LENGTH - LENGTHS_MARGIN
+		   && length < ONE_LENGTH + LENGTHS_MARGIN && recording) {
+		encodedBits[encodedBitsIndex++] = 1;
 
-	/* Zero */
-    } else if (length > ZERO_LENGTH - LENGTHS_MARGIN
-	       && length < ZERO_LENGTH + LENGTHS_MARGIN && recording) {
-	encodedBits[encodedBitsIndex++] = 0;
+		/* Zero */
+	} else if (length > ZERO_LENGTH - LENGTHS_MARGIN
+		   && length < ZERO_LENGTH + LENGTHS_MARGIN && recording) {
+		encodedBits[encodedBitsIndex++] = 0;
 
-	/* Sync bit - End of data package */
-    } else if (length > SYNCHRO_LENGTH - LENGTHS_MARGIN * 2
-	       && length < SYNCHRO_LENGTH + LENGTHS_MARGIN * 2
-	       && recording) {
-	decodeArray();
-	resetRecording();
-	recording = 1;
+		/* Sync bit - End of data package */
+	} else if (length > SYNCHRO_LENGTH - LENGTHS_MARGIN * 2
+		   && length < SYNCHRO_LENGTH + LENGTHS_MARGIN * 2
+		   && recording) {
+		decodeArray();
+		resetRecording();
+		recording = 1;
 
-	/* Signal length too long */
-    } else if (recording) {
-	resetRecording();
-    }
+		/* Signal length too long */
+	} else if (recording) {
+		resetRecording();
+	}
 }
 
 void printArray()
 {
-    if (encodedBitsIndex == 0)
-	return;
+	if (encodedBitsIndex == 0)
+		return;
 #if DEBUG > 1
-    int i;
-    struct tm *local;
-    time_t t = time(NULL);
-    local = localtime(&t);
+	int i;
+	struct tm *local;
+	time_t t = time(NULL);
+	local = localtime(&t);
 
-    fprintf(stderr, LANG_DATE_TIME, (local->tm_year + 1900),
-	    (local->tm_mon) + 1, local->tm_mday, local->tm_hour,
-	    local->tm_min, local->tm_sec);
+	fprintf(stderr, LANG_DATE_TIME, (local->tm_year + 1900),
+		(local->tm_mon) + 1, local->tm_mday, local->tm_hour,
+		local->tm_min, local->tm_sec);
 
-    for (i = 0; i < 36; i++)
-	fprintf(stderr, "%i", encodedBits[i]);
-    fprintf(stderr, "\n");
+	for (i = 0; i < 36; i++)
+		fprintf(stderr, "%i", encodedBits[i]);
+	fprintf(stderr, "\n");
 #endif
 }
 
 void decodeArray()
 {
-    if (encodedBitsIndex < 36) {
-	return;
-    }
-    printArray();
-    decodePluviometer();
-    decodeWindData();
+	if (encodedBitsIndex < 36) {
+		return;
+	}
+	printArray();
+	decodePluviometer();
+	decodeWindData();
 }
 
 void decodePluviometer()
 {
-    if (encodedBits[9] && encodedBits[10] && !encodedBits[11]
-	&& encodedBits[12] && encodedBits[13] && !encodedBits[14]
-	&& !encodedBits[15]) {
-	unsigned int rain = 0;
-	int i;
-	for (i = 16; i < 32; i++) {
-	    rain |= encodedBits[i] << (i - 16);
-	}
+	if (encodedBits[9] && encodedBits[10] && !encodedBits[11]
+	    && encodedBits[12] && encodedBits[13] && !encodedBits[14]
+	    && !encodedBits[15]) {
+		unsigned int rain = 0;
+		int i;
+		for (i = 16; i < 32; i++) {
+			rain |= encodedBits[i] << (i - 16);
+		}
 
-	printTime();
-	float rainFinal = (float) rain / 4;
-	printf(LANG_INFO_PLUVIOMETER, (float) rainFinal);
-	savePluviometer(rainFinal);
+		printTime();
+		float rainFinal = (float)rain / 4;
+		printf(LANG_INFO_PLUVIOMETER, (float)rainFinal);
+		savePluviometer(rainFinal);
 
-	if (encodedBits[8]) {
-	    printf(LANG_BATTERY_REPLACE);
-	} else {
-	    printf(LANG_BATTERY_OK);
+		if (encodedBits[8]) {
+			printf(LANG_BATTERY_REPLACE);
+		} else {
+			printf(LANG_BATTERY_OK);
+		}
 	}
-    }
 }
 
 void decodeWindData()
 {
-    /* Average Wind Speed */
-    if (encodedBits[9] && encodedBits[10] && encodedBits[12]
-	&& !encodedBits[13] && !encodedBits[14] && !encodedBits[15]
-	&& !encodedBits[16]
-	&& !encodedBits[17] && !encodedBits[18] && !encodedBits[19]
-	&& !encodedBits[20] && !encodedBits[21] && !encodedBits[22]
-	&& !encodedBits[23]) {
-	unsigned int windAverageSpeed = 0;
-	int i;
-	for (i = 24; i < 32; i++) {
-	    windAverageSpeed |= encodedBits[i] << (i - 24);
+	/* Average Wind Speed */
+	if (encodedBits[9] && encodedBits[10] && encodedBits[12]
+	    && !encodedBits[13] && !encodedBits[14] && !encodedBits[15]
+	    && !encodedBits[16]
+	    && !encodedBits[17] && !encodedBits[18] && !encodedBits[19]
+	    && !encodedBits[20] && !encodedBits[21] && !encodedBits[22]
+	    && !encodedBits[23]) {
+		unsigned int windAverageSpeed = 0;
+		int i;
+		for (i = 24; i < 32; i++) {
+			windAverageSpeed |= encodedBits[i] << (i - 24);
+		}
+
+		printTime();
+		printf(LANG_INFO_WIND_AVG, (float)windAverageSpeed / 5);
+
+		if (encodedBits[8]) {
+			printf(LANG_BATTERY_REPLACE);
+		} else {
+			printf(LANG_BATTERY_OK);
+		}
+
+		saveWind((float)windAverageSpeed / 5, -1.0, -1);
+
+		/* Wind gust & direction */
+	} else if (encodedBits[9] && encodedBits[10] && encodedBits[12]
+		   && encodedBits[13] && encodedBits[14]) {
+		unsigned int direction = 0;
+		unsigned int windGust = 0;
+		int i;
+		for (i = 15; i < 24; i++) {
+			direction |= encodedBits[i] << (i - 15);
+		}
+		for (i = 24; i < 32; i++) {
+			windGust |= encodedBits[i] << (i - 24);
+		}
+
+		printTime();
+		printf(LANG_INFO_WIND_DIR_GUST, direction, (float)windGust / 5);
+
+		if (encodedBits[8]) {
+			printf(LANG_BATTERY_REPLACE);
+		} else {
+			printf(LANG_BATTERY_OK);
+		}
+
+		saveWind(-1.0, (float)windGust / 5, direction);
+
+		/* Temperature & Humidity */
+	} else if (!encodedBits[9] || !encodedBits[10]) {
+		int temperature = 0;
+		int i;
+		for (i = 12; i < 23; i++) {
+			temperature |= encodedBits[i] << (i - 12);
+		}
+		if (encodedBits[23]) {
+			temperature = -2048 + temperature;
+		}
+		float temperatureFinal = (float)temperature / 10;
+
+		unsigned int humidityOnes = 0;
+		for (i = 24; i < 28; i++) {
+			humidityOnes |= encodedBits[i] << (i - 24);
+		}
+
+		unsigned int humidityTens = 0;
+		for (i = 28; i < 32; i++) {
+			humidityTens |= encodedBits[i] << (i - 28);
+		}
+
+		unsigned int humidity = humidityTens * 10 + humidityOnes;
+
+		printTime();
+		printf(LANG_INFO_TEMP_HUMIDITY, temperatureFinal, humidity);
+
+		if (encodedBits[8]) {
+			printf(LANG_BATTERY_REPLACE);
+		} else {
+			printf(LANG_BATTERY_OK);
+		}
+
+		if (combinedSensorChecksumConfirmed()) {
+			saveTemperature(temperatureFinal);
+			saveHumidity(humidity);
+		} else {
+			fprintf(stderr, LANG_WARNING_CRC);
+		}
 	}
-
-	printTime();
-	printf(LANG_INFO_WIND_AVG, (float) windAverageSpeed / 5);
-
-	if (encodedBits[8]) {
-	    printf(LANG_BATTERY_REPLACE);
-	} else {
-	    printf(LANG_BATTERY_OK);
-	}
-
-	saveWind((float) windAverageSpeed / 5, -1.0, -1);
-
-	/* Wind gust & direction */
-    } else if (encodedBits[9] && encodedBits[10] && encodedBits[12]
-	       && encodedBits[13] && encodedBits[14]) {
-	unsigned int direction = 0;
-	unsigned int windGust = 0;
-	int i;
-	for (i = 15; i < 24; i++) {
-	    direction |= encodedBits[i] << (i - 15);
-	}
-	for (i = 24; i < 32; i++) {
-	    windGust |= encodedBits[i] << (i - 24);
-	}
-
-	printTime();
-	printf(LANG_INFO_WIND_DIR_GUST, direction, (float) windGust / 5);
-
-	if (encodedBits[8]) {
-	    printf(LANG_BATTERY_REPLACE);
-	} else {
-	    printf(LANG_BATTERY_OK);
-	}
-
-	saveWind(-1.0, (float) windGust / 5, direction);
-
-	/* Temperature & Humidity */
-    } else if (!encodedBits[9] || !encodedBits[10]) {
-	int temperature = 0;
-	int i;
-	for (i = 12; i < 23; i++) {
-	    temperature |= encodedBits[i] << (i - 12);
-	}
-	if (encodedBits[23]) {
-	    temperature = -2048 + temperature;
-	}
-	float temperatureFinal = (float) temperature / 10;
-
-	unsigned int humidityOnes = 0;
-	for (i = 24; i < 28; i++) {
-	    humidityOnes |= encodedBits[i] << (i - 24);
-	}
-
-	unsigned int humidityTens = 0;
-	for (i = 28; i < 32; i++) {
-	    humidityTens |= encodedBits[i] << (i - 28);
-	}
-
-	unsigned int humidity = humidityTens * 10 + humidityOnes;
-
-	printTime();
-	printf(LANG_INFO_TEMP_HUMIDITY, temperatureFinal, humidity);
-
-	if (encodedBits[8]) {
-	    printf(LANG_BATTERY_REPLACE);
-	} else {
-	    printf(LANG_BATTERY_OK);
-	}
-
-	if (combinedSensorChecksumConfirmed()) {
-	    saveTemperature(temperatureFinal);
-	    saveHumidity(humidity);
-	} else {
-	    fprintf(stderr, LANG_WARNING_CRC);
-	}
-    }
 }
 
 bool combinedSensorChecksumConfirmed()
 {
-    unsigned char computedChecksum = 0x0F;
-    int i = 0, j = 0;
-    for (i = 0; i < 32; i += 4) {
-	unsigned char nibble = 0;
-	for (j = 0; j < 4; j++) {
-	    nibble |= encodedBits[i + j] << (j);
+	unsigned char computedChecksum = 0x0F;
+	int i = 0, j = 0;
+	for (i = 0; i < 32; i += 4) {
+		unsigned char nibble = 0;
+		for (j = 0; j < 4; j++) {
+			nibble |= encodedBits[i + j] << (j);
+		}
+		computedChecksum -= nibble;
 	}
-	computedChecksum -= nibble;
-    }
-    computedChecksum &= 0x0F;
+	computedChecksum &= 0x0F;
 
-    unsigned int readedChecksum = 0x00;
-    for (i = 32; i < 36; i++) {
-	readedChecksum |= encodedBits[i] << (i - 32);
-    }
+	unsigned int readedChecksum = 0x00;
+	for (i = 32; i < 36; i++) {
+		readedChecksum |= encodedBits[i] << (i - 32);
+	}
 
-    bool checksumsAreEqual = (readedChecksum == computedChecksum);
+	bool checksumsAreEqual = (readedChecksum == computedChecksum);
 
-    if (!checksumsAreEqual) {
-	printTime();
-	fprintf(stderr, LANG_INFO_CRC_DIFFER, readedChecksum,
-		computedChecksum, checksumsAreEqual);
-    }
+	if (!checksumsAreEqual) {
+		printTime();
+		fprintf(stderr, LANG_INFO_CRC_DIFFER, readedChecksum,
+			computedChecksum, checksumsAreEqual);
+	}
 
-    return checksumsAreEqual;
+	return checksumsAreEqual;
 }
 
 void printTime()
 {
-    struct tm *local;
-    time_t t;
+	struct tm *local;
+	time_t t;
 
-    t = time(NULL);
-    local = localtime(&t);
-    printf(LANG_DATE_TIME, (local->tm_year + 1900), (local->tm_mon) + 1,
-	   local->tm_mday, local->tm_hour, local->tm_min, local->tm_sec);
+	t = time(NULL);
+	local = localtime(&t);
+	printf(LANG_DATE_TIME, (local->tm_year + 1900), (local->tm_mon) + 1,
+	       local->tm_mday, local->tm_hour, local->tm_min, local->tm_sec);
 }
--- db.c
+++ db.c
@@ -14,8 +14,7 @@
 #define WIND_SAMPLES 120	/* WIND_SAMPLES = WIND_SAVE_INTERVALL / 30 */
 
 #ifdef LANGUAGE_ENGLISH
-static const char LANG_DB_ERROR_OPENING[] =
-    "ERROR: Can not open database!";
+static const char LANG_DB_ERROR_OPENING[] = "ERROR: Can not open database!";
 static const char LANG_DB_PLUVIOMETER_QUERY[] =
     "\nERROR in Pluviometer query: SQLite returned Error Code: %i.\n";
 static const char LANG_DB_TEMP_QUERY[] =
@@ -47,19 +46,18 @@ static const char LANG_DB_WIND_QUERY[] =
 #endif
 
 static const char *SQL_CREATE_TABLE[] = {
-    "CREATE TABLE IF NOT EXISTS pluviometer( created DATETIME, amount  DECIMAL(10,2));",
-    "CREATE TABLE IF NOT EXISTS temperature( created DATETIME, amount  DECIMAL(4,1));",
-    "CREATE TABLE IF NOT EXISTS humidity( created DATETIME, amount  TINYINT);",
-    "CREATE TABLE IF NOT EXISTS wind( created DATETIME, speed DECIMAL(3,1), gust DECIMAL(3,1), direction SMALLINT );"
+	"CREATE TABLE IF NOT EXISTS pluviometer( created DATETIME, amount  DECIMAL(10,2));",
+	"CREATE TABLE IF NOT EXISTS temperature( created DATETIME, amount  DECIMAL(4,1));",
+	"CREATE TABLE IF NOT EXISTS humidity( created DATETIME, amount  TINYINT);",
+	"CREATE TABLE IF NOT EXISTS wind( created DATETIME, speed DECIMAL(3,1), gust DECIMAL(3,1), direction SMALLINT );"
 };
 
 static const char *SQL_CREATE_INDEX[] = {
-    "CREATE INDEX IF NOT EXISTS ix_pluviometer_created ON pluviometer(created);",
-    "CREATE INDEX IF NOT EXISTS ix_temperature_created ON temperature(created);",
-    "CREATE INDEX IF NOT EXISTS ix_humidity_created ON humidity(created);",
-    "CREATE INDEX IF NOT EXISTS ix_wind_created ON wind(created);"
+	"CREATE INDEX IF NOT EXISTS ix_pluviometer_created ON pluviometer(created);",
+	"CREATE INDEX IF NOT EXISTS ix_temperature_created ON temperature(created);",
+	"CREATE INDEX IF NOT EXISTS ix_humidity_created ON humidity(created);",
+	"CREATE INDEX IF NOT EXISTS ix_wind_created ON wind(created);"
 };
-
 
 sqlite3 *conn;
 int error = 0;
@@ -68,226 +66,226 @@ time_t t;
 
 void initializeDatabase()
 {
-    char *errMsg = 0;
-    int i;
-    /* Open database */
-    error = sqlite3_open(DB_FILENAME, &conn);
-    if (error) {
-	fprintf(stderr, LANG_DB_ERROR_OPENING);
-	exit(3);
-    }
-    /* Create database tables and indices, if not exits */
-    for (i = 0; i < 4; i++) {
-	error = sqlite3_exec(conn, SQL_CREATE_TABLE[i], 0, 0, &errMsg);
-	if (error != SQLITE_OK) {
-	    fprintf(stderr, LANG_DB_CREATE_TBL, errMsg,
-		    SQL_CREATE_TABLE[i]);
-	    exit(4);
+	char *errMsg = 0;
+	int i;
+	/* Open database */
+	error = sqlite3_open(DB_FILENAME, &conn);
+	if (error) {
+		fprintf(stderr, LANG_DB_ERROR_OPENING);
+		exit(3);
 	}
+	/* Create database tables and indices, if not exits */
+	for (i = 0; i < 4; i++) {
+		error = sqlite3_exec(conn, SQL_CREATE_TABLE[i], 0, 0, &errMsg);
+		if (error != SQLITE_OK) {
+			fprintf(stderr, LANG_DB_CREATE_TBL, errMsg,
+				SQL_CREATE_TABLE[i]);
+			exit(4);
+		}
 
-	error = sqlite3_exec(conn, SQL_CREATE_INDEX[i], 0, 0, &errMsg);
-	if (error != SQLITE_OK) {
-	    fprintf(stderr, LANG_DB_CREATE_TBL, errMsg,
-		    SQL_CREATE_INDEX[i]);
-	    exit(4);
+		error = sqlite3_exec(conn, SQL_CREATE_INDEX[i], 0, 0, &errMsg);
+		if (error != SQLITE_OK) {
+			fprintf(stderr, LANG_DB_CREATE_TBL, errMsg,
+				SQL_CREATE_INDEX[i]);
+			exit(4);
+		}
 	}
-    }
 }
 
 void savePluviometer(float amount)
 {
-    static signed int old_hour = -1;
-    static float old_value = -FLT_MAX;
+	static signed int old_hour = -1;
+	static float old_value = -FLT_MAX;
 
-    t = time(NULL);
-    local = localtime(&t);
+	t = time(NULL);
+	local = localtime(&t);
 
-    /* Store if value has changed or older than an hour */
-    if (old_hour != local->tm_hour || old_value < amount) {
-	char query[1024] = " ";
-	sprintf(query,
-		"INSERT INTO pluviometer VALUES (datetime('now', 'localtime'), %.2f);",
-		amount);
-	error = sqlite3_exec(conn, query, 0, 0, 0);
-	if (error != SQLITE_OK) {
-	    fprintf(stderr, LANG_DB_PLUVIOMETER_QUERY, error);
-	    exit(5);
+	/* Store if value has changed or older than an hour */
+	if (old_hour != local->tm_hour || old_value < amount) {
+		char query[1024] = " ";
+		sprintf(query,
+			"INSERT INTO pluviometer VALUES (datetime('now', 'localtime'), %.2f);",
+			amount);
+		error = sqlite3_exec(conn, query, 0, 0, 0);
+		if (error != SQLITE_OK) {
+			fprintf(stderr, LANG_DB_PLUVIOMETER_QUERY, error);
+			exit(5);
+		}
 	}
-    }
 
-    old_hour = local->tm_hour;
-    old_value = amount;
+	old_hour = local->tm_hour;
+	old_value = amount;
 }
 
 void saveTemperature(float temperature)
 {
-    static signed int t_old_min = -1;
-    static float old_value = -FLT_MAX;
+	static signed int t_old_min = -1;
+	static float old_value = -FLT_MAX;
 
-    t = time(NULL);
-    local = localtime(&t);
+	t = time(NULL);
+	local = localtime(&t);
 
-    /* Store if value has changed or not from same minute */
-    if (t_old_min != local->tm_min || old_value != temperature) {
+	/* Store if value has changed or not from same minute */
+	if (t_old_min != local->tm_min || old_value != temperature) {
 
-	/* Check for invalid values */
-	float difference = old_value - temperature;
-	if ((difference < -TEMP_DIFF || difference > TEMP_DIFF)
-	    && old_value != -FLT_MAX) {
-	    printf(LANG_DB_TEMP_DIFF, old_value, temperature);
-	    return;
+		/* Check for invalid values */
+		float difference = old_value - temperature;
+		if ((difference < -TEMP_DIFF || difference > TEMP_DIFF)
+		    && old_value != -FLT_MAX) {
+			printf(LANG_DB_TEMP_DIFF, old_value, temperature);
+			return;
+		}
+
+		char query[1024] = " ";
+		sprintf(query,
+			"INSERT INTO temperature VALUES (datetime('now', 'localtime'), %.1f);",
+			temperature);
+		error = sqlite3_exec(conn, query, 0, 0, 0);
+
+		if (error != SQLITE_OK) {
+			fprintf(stderr, LANG_DB_TEMP_QUERY, error);
+			exit(6);
+		}
 	}
 
-	char query[1024] = " ";
-	sprintf(query,
-		"INSERT INTO temperature VALUES (datetime('now', 'localtime'), %.1f);",
-		temperature);
-	error = sqlite3_exec(conn, query, 0, 0, 0);
-
-	if (error != SQLITE_OK) {
-	    fprintf(stderr, LANG_DB_TEMP_QUERY, error);
-	    exit(6);
-	}
-    }
-
-    t_old_min = local->tm_min;
-    old_value = temperature;
+	t_old_min = local->tm_min;
+	old_value = temperature;
 }
 
 void saveHumidity(unsigned int humidity)
 {
-    static signed int h_old_min = -1;
-    static float old_value = -FLT_MAX;
+	static signed int h_old_min = -1;
+	static float old_value = -FLT_MAX;
 
-    t = time(NULL);
-    local = localtime(&t);
+	t = time(NULL);
+	local = localtime(&t);
 
-    /* Store if value has changed or not from same minute */
-    if (h_old_min != local->tm_min || old_value != humidity) {
+	/* Store if value has changed or not from same minute */
+	if (h_old_min != local->tm_min || old_value != humidity) {
 
-	/* Check for invalid values */
-	if (humidity <= 0 || humidity > 100) {
-	    fprintf(stderr, LANG_DB_HUMID_DIFF, humidity);
-	    return;
+		/* Check for invalid values */
+		if (humidity <= 0 || humidity > 100) {
+			fprintf(stderr, LANG_DB_HUMID_DIFF, humidity);
+			return;
+		}
+
+		char query[1024] = " ";
+		sprintf(query,
+			"INSERT INTO humidity VALUES (datetime('now', 'localtime'), %i);",
+			humidity);
+		error = sqlite3_exec(conn, query, 0, 0, 0);
+
+		if (error != SQLITE_OK) {
+			fprintf(stderr, LANG_DB_PLUVIOMETER_QUERY, error);
+			exit(7);
+		}
 	}
 
-	char query[1024] = " ";
-	sprintf(query,
-		"INSERT INTO humidity VALUES (datetime('now', 'localtime'), %i);",
-		humidity);
-	error = sqlite3_exec(conn, query, 0, 0, 0);
-
-	if (error != SQLITE_OK) {
-	    fprintf(stderr, LANG_DB_PLUVIOMETER_QUERY, error);
-	    exit(7);
-	}
-    }
-
-    h_old_min = local->tm_min;
-    old_value = humidity;
+	h_old_min = local->tm_min;
+	old_value = humidity;
 }
 
 void saveWind(float speed, float gust, unsigned int dir)
 {
-    static time_t temp_time = 0;
-    static time_t old_time = 0;
-    static signed int counter = 0;
-    static float windSpeed[WIND_SAMPLES] = { -1.0 };
-    static float windGust[WIND_SAMPLES] = { -1.0 };
-    static int windDir[WIND_SAMPLES] = { -1 };
-    static int rowid = 0;
-    static int saved = 0;
+	static time_t temp_time = 0;
+	static time_t old_time = 0;
+	static signed int counter = 0;
+	static float windSpeed[WIND_SAMPLES] = { -1.0 };
+	static float windGust[WIND_SAMPLES] = { -1.0 };
+	static int windDir[WIND_SAMPLES] = { -1 };
+	static int rowid = 0;
+	static int saved = 0;
 
-    float x, y, rad;
-    unsigned int i;
+	float x, y, rad;
+	unsigned int i;
 
-    time(&t);
+	time(&t);
 
-    /* Check a new reading (aprox. every 30s). Discard incomplete data */
-    if (difftime(t, temp_time) > 20.0) {
-	if (windSpeed[counter] > -1.0 && windGust[counter] > -1.0)
-	    counter++;
+	/* Check a new reading (aprox. every 30s). Discard incomplete data */
+	if (difftime(t, temp_time) > 20.0) {
+		if (windSpeed[counter] > -1.0 && windGust[counter] > -1.0)
+			counter++;
+		i = counter % WIND_SAMPLES;
+		windSpeed[i] = -1.0;
+		windGust[i] = -1.0;
+		windDir[i] = -1;
+		temp_time = time(NULL);
+		saved = 0;
+	}
+
+	/* Store current wind data */
 	i = counter % WIND_SAMPLES;
-	windSpeed[i] = -1.0;
-	windGust[i] = -1.0;
-	windDir[i] = -1;
-	temp_time = time(NULL);
-	saved = 0;
-    }
+	if (speed > -1.0) {
+		windSpeed[i] = speed;
+	} else if (gust > -1.0) {
+		windGust[i] = gust;
+		windDir[i] = dir;
+	}
 
-    /* Store current wind data */
-    i = counter % WIND_SAMPLES;
-    if (speed > -1.0) {
-	windSpeed[i] = speed;
-    } else if (gust > -1.0) {
-	windGust[i] = gust;
-	windDir[i] = dir;
-    }
+	/* Return if only one value available or already saved */
+	if (windSpeed[i] < 0.0 || windGust[i] < 0.0 || saved == 1)
+		return;
 
-    /* Return if only one value available or already saved */
-    if (windSpeed[i] < 0.0 || windGust[i] < 0.0 || saved == 1)
-	return;
-
-    /* Calculate averages 
-     * Wind dir from http://www.control.com/thread/1026210133
-     * By M.A.Saghafi on 11 October, 2010 - 8:26 am and M Barnes on 18 May, 2011 - 6:48 am */
-    ++counter;
-    speed = 0.0;
-    for (i = 0; i < counter; i++) {
+	/* Calculate averages 
+	 * Wind dir from http://www.control.com/thread/1026210133
+	 * By M.A.Saghafi on 11 October, 2010 - 8:26 am and M Barnes on 18 May, 2011 - 6:48 am */
+	++counter;
+	speed = 0.0;
+	for (i = 0; i < counter; i++) {
 #if DEBUG > 2
-	fprintf(stderr,
-		"[AVG] %i: Speed: %.1f\tGust: %.1f\tDir: %i\tc: %i\n", i,
-		windSpeed[i], windGust[i], windDir[i], counter);
+		fprintf(stderr,
+			"[AVG] %i: Speed: %.1f\tGust: %.1f\tDir: %i\tc: %i\n",
+			i, windSpeed[i], windGust[i], windDir[i], counter);
 #endif
-	dir = i % WIND_SAMPLES;
-	rad = M_PI / 180 * windDir[dir];
-	x += -windSpeed[dir] * sin(rad);
-	y += -windSpeed[dir] * cos(rad);
-	speed += windSpeed[dir];
-	if (windGust[dir] > gust)
-	    gust = windGust[dir];
-    }
+		dir = i % WIND_SAMPLES;
+		rad = M_PI / 180 * windDir[dir];
+		x += -windSpeed[dir] * sin(rad);
+		y += -windSpeed[dir] * cos(rad);
+		speed += windSpeed[dir];
+		if (windGust[dir] > gust)
+			gust = windGust[dir];
+	}
 
-    speed = speed / counter;
-    x = x / counter;
-    y = y / counter;
+	speed = speed / counter;
+	x = x / counter;
+	y = y / counter;
 
-    if (x == 0)
-	dir = 0;
-    else if (x > 0)
-	dir = 270 - 180 / M_PI * atan(y / x);
-    else
-	dir = 90 - 180 / M_PI * atan(y / x);
-    dir = dir % 360;
+	if (x == 0)
+		dir = 0;
+	else if (x > 0)
+		dir = 270 - 180 / M_PI * atan(y / x);
+	else
+		dir = 90 - 180 / M_PI * atan(y / x);
+	dir = dir % 360;
 
-    /* Update row if time < WIND_SAVE_INTERVALL */
-    char query[128] = " ";
-    if (difftime(t, old_time) < WIND_SAVE_INTERVALL && rowid > 0) {
-	sprintf(query,
-		"UPDATE wind SET created=datetime('now','localtime'), speed=%.1f, gust=%.1f, direction=%i WHERE rowid=%i;",
-		speed, gust, dir, rowid);
-	error = sqlite3_exec(conn, query, 0, 0, 0);
-	if (error != SQLITE_OK)
-	    fprintf(stderr, LANG_DB_WIND_QUERY, error, query);
+	/* Update row if time < WIND_SAVE_INTERVALL */
+	char query[128] = " ";
+	if (difftime(t, old_time) < WIND_SAVE_INTERVALL && rowid > 0) {
+		sprintf(query,
+			"UPDATE wind SET created=datetime('now','localtime'), speed=%.1f, gust=%.1f, direction=%i WHERE rowid=%i;",
+			speed, gust, dir, rowid);
+		error = sqlite3_exec(conn, query, 0, 0, 0);
+		if (error != SQLITE_OK)
+			fprintf(stderr, LANG_DB_WIND_QUERY, error, query);
 
-	--counter;
-	/* Insert a new row */
-    } else {
-	sprintf(query,
-		"INSERT INTO wind VALUES (datetime('now', 'localtime'), %.1f, %.1f, %i);",
-		speed, gust, dir);
-	error = sqlite3_exec(conn, query, 0, 0, 0);
-	if (error != SQLITE_OK)
-	    fprintf(stderr, LANG_DB_WIND_QUERY, error, query);
+		--counter;
+		/* Insert a new row */
+	} else {
+		sprintf(query,
+			"INSERT INTO wind VALUES (datetime('now', 'localtime'), %.1f, %.1f, %i);",
+			speed, gust, dir);
+		error = sqlite3_exec(conn, query, 0, 0, 0);
+		if (error != SQLITE_OK)
+			fprintf(stderr, LANG_DB_WIND_QUERY, error, query);
 
-	rowid = (int) sqlite3_last_insert_rowid(conn);
-	counter = 0;
-	old_time = time(NULL);
-	windGust[0] = -1.0;
-	windDir[0] = -1.0;
-    }
-    saved = 1;
+		rowid = (int)sqlite3_last_insert_rowid(conn);
+		counter = 0;
+		old_time = time(NULL);
+		windGust[0] = -1.0;
+		windDir[0] = -1.0;
+	}
+	saved = 1;
 #if DEBUG > 1
-    fprintf(stderr, "Query: %s\n", query);
+	fprintf(stderr, "Query: %s\n", query);
 #endif
 }
